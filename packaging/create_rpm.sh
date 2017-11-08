c5574185f3fe74cb78b1078f76b7e40d9f6de220
Pavel Brodsky <mcouthon@gmail.com>
2017-11-08 23:44:43 +0200

    Prettify `print_line`

--- packaging/create_rpm.sh
+++ packaging/create_rpm.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+LINE="-------------------------------------------------------------"
+
 function print_line() {
-  echo '-------------------------------------------------------------'
-  echo $1
-  echo '-------------------------------------------------------------'
+  echo -e "$COL_YELLOW $LINE $COL_RESET"
+  echo -e "$COL_BLUE $1 $COL_RESET"
+  echo -e "$COL_YELLOW $LINE $COL_RESET"
 }
 
 CLOUDIFY_RELEASE_URL="http://cloudify-release-eu.s3.amazonaws.com/cloudify"
