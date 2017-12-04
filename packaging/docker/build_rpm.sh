5012efcdcdd64c1804b7c997fffc19ac7a5d71c2
asdf <tehasdf@gmail.com>
2017-12-04 14:21:26 +0100

    Allow parametrizing REMOTE_PATH

--- packaging/docker/build_rpm.sh
+++ packaging/docker/build_rpm.sh
@@ -2,10 +2,8 @@
 
 set -eux
 
-REMOTE_LOCATION='/root/cloudify-manager-install'
-
 echo "Creating install RPM..."
-chmod +x ${REMOTE_LOCATION}/packaging/create_rpm.sh
-${REMOTE_LOCATION}/packaging/create_rpm.sh community true master ${REMOTE_LOCATION}
+chmod +x ${REMOTE_PATH:=/root/cloudify-manager-install}/packaging/create_rpm.sh
+${REMOTE_PATH}/packaging/create_rpm.sh community true master ${REMOTE_PATH}
 rpm -i /tmp/cloudify-manager-install*.rpm
 rm -f /tmp/cloudify-manager-install*.rpm
