6a9a7c776b181becb770b08aba8d05718a51e0d9
asdf <tehasdf@gmail.com>
2017-12-04 14:20:36 +0100

    build_rpm.sh: don't remove REMOTE_LOCATION

--- packaging/docker/build_rpm.sh
+++ packaging/docker/build_rpm.sh
@@ -9,4 +9,3 @@ chmod +x ${REMOTE_LOCATION}/packaging/create_rpm.sh
 ${REMOTE_LOCATION}/packaging/create_rpm.sh community true master ${REMOTE_LOCATION}
 rpm -i /tmp/cloudify-manager-install*.rpm
 rm -f /tmp/cloudify-manager-install*.rpm
-rm -rf ${REMOTE_LOCATION}
