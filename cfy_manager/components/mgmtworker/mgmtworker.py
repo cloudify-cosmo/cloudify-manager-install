7c5d6fabe036fdae54a3896ebd2e8df831d783a3
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2018-01-03 18:39:32 +0100

    CFY-7576 Use new mgmtworker rpm (#88)

--- cfy_manager/components/mgmtworker/mgmtworker.py
+++ cfy_manager/components/mgmtworker/mgmtworker.py
@@ -33,10 +33,9 @@ from ... import constants as const
 from ...exceptions import ValidationError
 
 from ...utils import common
+from ...utils.files import deploy
 from ...utils.systemd import systemd
-from ...utils.logrotate import set_logrotate
 from ...utils.install import yum_install, yum_remove
-from ...utils.files import remove_files, deploy, copy_notice, remove_notice
 
 HOME_DIR = '/opt/mgmtworker'
 MGMTWORKER_VENV = join(HOME_DIR, 'env')
@@ -58,20 +57,9 @@ def _install():
     # utils.chmod('770', riemann_dir)
 
 
-def _create_paths():
-    common.mkdir(HOME_DIR)
-    common.mkdir(join(HOME_DIR, 'config'))
-    common.mkdir(join(HOME_DIR, 'work'))
-    common.mkdir(LOG_DIR)
-
-    common.chown(const.CLOUDIFY_USER, const.CLOUDIFY_GROUP, LOG_DIR)
-    common.chown(const.CLOUDIFY_USER, const.CLOUDIFY_GROUP, HOME_DIR)
-
+def _deploy_mgmtworker_config():
     config[MGMTWORKER][HOME_DIR_KEY] = HOME_DIR
     config[MGMTWORKER][LOG_DIR_KEY] = LOG_DIR
-
-
-def _deploy_mgmtworker_config():
     config[MGMTWORKER][SERVICE_USER] = const.CLOUDIFY_USER
     config[MGMTWORKER][SERVICE_GROUP] = const.CLOUDIFY_GROUP
 
@@ -85,17 +73,6 @@ def _deploy_mgmtworker_config():
     # The config contains credentials, do not let the world read it
     common.chmod('440', broker_config_dst)
     common.chown(const.CLOUDIFY_USER, const.CLOUDIFY_GROUP, broker_config_dst)
-
-
-def _configure_logging():
-    logger.info('Configuring Management worker logging...')
-    logging_config_dir = '/etc/cloudify'
-    common.mkdir(logging_config_dir)
-
-    common.copy(
-        source=join(CONFIG_PATH, 'logging.conf'),
-        destination=join(logging_config_dir, 'logging.conf')
-    )
 
 
 def _prepare_snapshot_permissions():
@@ -139,12 +116,8 @@ def _start_and_verify_mgmtworker():
 
 
 def _configure():
-    copy_notice(MGMTWORKER)
-    _create_paths()
     _deploy_mgmtworker_config()
     systemd.configure(MGMTWORKER)
-    set_logrotate(MGMTWORKER)
-    _configure_logging()
     _prepare_snapshot_permissions()
     _start_and_verify_mgmtworker()
 
@@ -164,8 +137,6 @@ def configure():
 
 def remove():
     logger.notice('Removing Management Worker...')
-    remove_notice(MGMTWORKER)
-    systemd.remove(MGMTWORKER)
-    remove_files([HOME_DIR, LOG_DIR])
+    systemd.remove(MGMTWORKER, service_file=False)
     yum_remove('cloudify-management-worker')
     logger.notice('Management Worker successfully removed')
