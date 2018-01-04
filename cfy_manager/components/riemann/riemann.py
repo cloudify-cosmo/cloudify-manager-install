82ae95dbe98ed73e15d44f8ba6ba61d3258bfda0
Matt Wheeler <m@funkyhat.org>
2018-01-04 17:01:02 +0000

    CFY-7611 new cloudify-riemann RPM

--- cfy_manager/components/riemann/riemann.py
+++ cfy_manager/components/riemann/riemann.py
@@ -15,7 +15,7 @@
 
 from os.path import join
 
-from .. import SCRIPTS, SOURCES, CONFIG, SERVICE_USER, SERVICE_GROUP
+from .. import SCRIPTS, SOURCES, CONFIG
 
 from ..service_names import RIEMANN
 
@@ -26,8 +26,7 @@ from ...logger import get_logger
 from ...utils import common, files
 from ...utils.systemd import systemd
 from ...utils.install import yum_install, yum_remove
-from ...utils.logrotate import set_logrotate, remove_logrotate
-from ...utils.users import create_service_user
+
 
 logger = get_logger(RIEMANN)
 
@@ -35,47 +34,14 @@ HOME_DIR = join('/opt', RIEMANN)
 CONFIG_PATH = join('/etc', RIEMANN)
 SCRIPTS_PATH = join(constants.COMPONENTS_DIR, RIEMANN, SCRIPTS)
 LOG_DIR = join(constants.BASE_LOG_DIR, RIEMANN)
-LANGOHR_HOME = '/opt/lib'
-LANGOHR_JAR_PATH = join(LANGOHR_HOME, 'langohr.jar')
-
-
-def _create_paths():
-    common.mkdir(HOME_DIR)
-    common.mkdir(LOG_DIR)
-    common.mkdir(LANGOHR_HOME)
-    common.mkdir(CONFIG_PATH)
-    common.mkdir('{0}/conf.d'.format(CONFIG_PATH))
-
-    # Need to allow access to mgmtworker (thus cfyuser)
-    common.chown(RIEMANN, constants.CLOUDIFY_GROUP, HOME_DIR)
-    common.chmod('770', HOME_DIR)
-    common.chown(RIEMANN, RIEMANN, LOG_DIR)
 
 
 def _install():
     sources = config[RIEMANN][SOURCES]
 
-    langohr_resource_path = \
-        files.get_local_source_path(sources['langohr_source_url'])
-    common.copy(langohr_resource_path, LANGOHR_JAR_PATH)
-    common.chmod('644', LANGOHR_JAR_PATH)
-
     yum_install(sources['daemonize_source_url'])
     yum_install(sources['riemann_source_url'])
-
-
-def _configure_riemann():
-    logger.debug('Getting manager repo archive...')
-    cloudify_resources_url = config[RIEMANN][SOURCES]['cloudify_resources_url']
-    local_tar_path = files.get_local_source_path(cloudify_resources_url)
-    manager_dir = common.untar(local_tar_path, unique_tmp_dir=True)
-
-    logger.debug('Deploying Riemann manager.config...')
-    config_src_path = join(
-        manager_dir, 'plugins', 'riemann-controller',
-        'riemann_controller', 'resources', 'manager.config'
-    )
-    common.move(config_src_path, join(CONFIG_PATH, 'conf.d', 'manager.config'))
+    yum_install(sources['cloudify_riemann_url'])
 
 
 def _deploy_riemann_config():
@@ -106,20 +72,8 @@ def _start_and_verify_service():
     systemd.verify_alive(RIEMANN)
 
 
-def _create_user():
-    create_service_user(RIEMANN, RIEMANN, home=constants.CLOUDIFY_HOME_DIR)
-
-    # Used in the service template
-    config[RIEMANN][SERVICE_USER] = RIEMANN
-    config[RIEMANN][SERVICE_GROUP] = RIEMANN
-
-
 def _configure():
     files.copy_notice(RIEMANN)
-    _create_user()
-    _create_paths()
-    set_logrotate(RIEMANN)
-    _configure_riemann()
     _deploy_riemann_config()
     _deploy_riemann_activation_script()
     _start_and_verify_service()
@@ -140,14 +94,6 @@ def configure():
 
 def remove():
     logger.notice('Removing Riemann...')
-    files.remove_notice(RIEMANN)
-    systemd.remove(RIEMANN)
-    remove_logrotate(RIEMANN)
-    files.remove_files([
-        HOME_DIR,
-        CONFIG_PATH,
-        LOG_DIR,
-        LANGOHR_JAR_PATH,
-    ])
+    systemd.remove(RIEMANN, service_file=False)
     yum_remove(RIEMANN)
     logger.notice('Riemann successfully removed')
