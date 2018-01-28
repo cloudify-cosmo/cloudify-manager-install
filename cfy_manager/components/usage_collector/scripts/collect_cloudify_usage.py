46c97d1c7aec61bd7e5c7969c1561f96789171b2
Omer Duskin <omerd@gigaspaces.com>
2018-01-28 12:26:22 +0200

    CFY-7828 bigquery doesn't accept dash

--- cfy_manager/components/usage_collector/scripts/collect_cloudify_usage.py
+++ cfy_manager/components/usage_collector/scripts/collect_cloudify_usage.py
@@ -55,20 +55,24 @@ def _find_substring_in_list(str_list, substring):
 
 
 def _collect_metadata(data):
+    pkg_distribution = pkg_resources.get_distribution('cloudify-rest-service')
+    manager_version = pkg_distribution.version
     with open(MANAGER_ID_PATH) as id_file:
         manager_id = id_file.read().strip()
     data['metadata'] = {
-        'manager-id': manager_id
+        'manager_id': manager_id,
+        'premium_edition': config.instance.edition.lower() == 'premium',
+        'version': manager_version
     }
 
 
 def _collect_system_data(data):
     sys_tech = platform().lower()
     data['system'] = {
-        'centos-os': 'centos' in sys_tech,
-        'redhat-os': 'redhat' in sys_tech,
-        'cpu-count': cpu_count(),
-        'mem-size-gb':
+        'centos_os': 'centos' in sys_tech,
+        'redhat_os': 'redhat' in sys_tech,
+        'cpu_count': cpu_count(),
+        'mem_size_gb':
             sysconf('SC_PAGE_SIZE') * sysconf('SC_PHYS_PAGES') / GIGA_SIZE
     }
 
@@ -77,42 +81,38 @@ def _collect_cloudify_data(data):
     with _get_storage_manager() as sm:
         plugins_list = [plugin.package_name.lower()
                         for plugin in sm.list(models.Plugin, all_tenants=True)]
-        data['cloudify-usage'] = {
-            'tenants-count': len(sm.list(models.Tenant)),
-            'users-count': len(sm.list(models.User)),
-            'usergroups-count': len(sm.list(models.Group)),
-            'blueprints-count': len(sm.list(models.Blueprint,
+        data['cloudify_usage'] = {
+            'tenants_count': len(sm.list(models.Tenant)),
+            'users_count': len(sm.list(models.User)),
+            'usergroups_count': len(sm.list(models.Group)),
+            'blueprints_count': len(sm.list(models.Blueprint,
                                             all_tenants=True)),
-            'deployments-count': len(sm.list(models.Deployment,
+            'deployments_count': len(sm.list(models.Deployment,
                                              all_tenants=True)),
-            'executions-count': len(sm.list(models.Execution,
+            'executions_count': len(sm.list(models.Execution,
                                             all_tenants=True)),
-            'secrets-count': len(sm.list(models.Secret, all_tenants=True)),
-            'nodes-count': len(sm.list(models.Node, all_tenants=True)),
-            'node-instances_count': len(sm.list(models.NodeInstance,
+            'secrets_count': len(sm.list(models.Secret, all_tenants=True)),
+            'nodes_count': len(sm.list(models.Node, all_tenants=True)),
+            'node_instances_count': len(sm.list(models.NodeInstance,
                                                 all_tenants=True)),
-            'plugins-count': len(plugins_list),
-            'aws-plugin': _find_substring_in_list(plugins_list, 'aws'),
-            'azure-plugin': _find_substring_in_list(plugins_list, 'azure'),
-            'gcp-plugin': _find_substring_in_list(plugins_list, 'gcp'),
-            'openstack-plugin': _find_substring_in_list(plugins_list,
+            'plugins_count': len(plugins_list),
+            'aws_plugin': _find_substring_in_list(plugins_list, 'aws'),
+            'azure_plugin': _find_substring_in_list(plugins_list, 'azure'),
+            'gcp_plugin': _find_substring_in_list(plugins_list, 'gcp'),
+            'openstack_plugin': _find_substring_in_list(plugins_list,
                                                         'openstack')
         }
 
 
 def _collect_cloudify_config(data):
-    manager_version = pkg_resources.get_distribution('cloudify-rest-service') \
-        .version
     config.instance.load_from_file(RESTSERVICE_CONFIG_PATH)
     app = server.CloudifyFlaskApp()
     try:
         with app.app_context():
             ldap = bool(app.ldap)
-        data['cloudify-config'] = {
-            'ldap-enabled': ldap,
-            'ha-enabled': _is_clustered(),
-            'premium-edition': config.instance.edition.lower() == 'premium',
-            'version': manager_version
+        data['cloudify_config'] = {
+            'ldap_enabled': ldap,
+            'ha_enabled': _is_clustered()
         }
     finally:
         config.reset(config.Config())
