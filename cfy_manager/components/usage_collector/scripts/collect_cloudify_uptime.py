b5e457975c36a3472dd316641d6b70ee24c2af10
Omer Duskin <omerd@gigaspaces.com>
2018-01-30 17:41:00 +0200

    CFY-7841 fix uptime send data

--- cfy_manager/components/usage_collector/scripts/collect_cloudify_uptime.py
+++ cfy_manager/components/usage_collector/scripts/collect_cloudify_uptime.py
@@ -1,7 +1,11 @@
-
+import json
 from os import path
 from uuid import uuid4
 from requests import post
+
+import pkg_resources
+
+from manager_rest import config
 
 try:
     from cloudify_premium.ha import node_status
@@ -15,12 +19,20 @@ CLOUDIFY_ENDPOINT_UPTIME_URL = 'https://api.cloudify.co/cloudifyUptime'
 
 
 def _collect_metadata(data):
+    pkg_distribution = pkg_resources.get_distribution('cloudify-rest-service')
+    manager_version = pkg_distribution.version
     with open(MANAGER_ID_PATH) as id_file:
         manager_id = id_file.read().strip()
-    data['metadata'] = {'manager_id': manager_id}
+    data['metadata'] = {
+        'manager_id': manager_id,
+        'premium_edition': config.instance.edition.lower() == 'premium',
+        'version': manager_version
+    }
 
 
 def _send_data(data):
+    # for some reason, multi hierarchy dict doesn't pass well to the end point
+    data = {'data': json.dumps(data)}
     post(CLOUDIFY_ENDPOINT_UPTIME_URL, data=data)
 
 
