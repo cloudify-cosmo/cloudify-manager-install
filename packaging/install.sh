3094a0387a1292c530b688241166ea916037b1e1
Matt Wheeler <m@funkyhat.org>
2018-01-31 15:41:04 +0000

    CFY-7836 fix config.yaml ownership

--- packaging/install.sh
+++ packaging/install.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 
 echo "###################################################################"
-echo "Installing cloudify manager installer..."
-echo "###################################################################"
-
-CONFIG_FILE=/etc/cloudify/config.yaml
-
-# Using $SUDO_USER instead of $USER here because fpm runs the script as sudo
-# and needs access to the config file as the user
-sudo chown $SUDO_USER:$SUDO_USER ${CONFIG_FILE}
-
-echo "###################################################################"
 echo "Cloudify installer is ready!"
 echo "Edit ${CONFIG_FILE}, and run cfy_manager install"
+echo "(members of group wheel have write access)
 echo "###################################################################"
