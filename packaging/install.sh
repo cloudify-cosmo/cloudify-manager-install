4c3b2ee05765acc5cfa5dea4c51dff794b128e1f
Pavel Brodsky <mcouthon@gmail.com>
2018-02-05 16:33:53 +0200

    CFY-7859 Make the post RPM install message more user friendly

--- packaging/install.sh
+++ packaging/install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-echo "###################################################################"
-echo "Cloudify installer is ready!"
-echo "Edit /etc/cloudify/config.yaml, and run cfy_manager install"
-echo "(members of group wheel have write access)"
-echo "###################################################################"
+echo "
+###########################################################################
+Cloudify installer is ready!
+To use it run:
+cfy_manager install --private-ip <PRIVATE_IP> --public-ip <PUBLIC_IP>
+(Use cfy_manager -h for a full list of options)
+
+Alternatively, edit /etc/cloudify/config.yaml to set IPs and other options,
+and then run:
+cfy_manager install
+
+(Members of the 'wheel' group have write-level access to this config file)
+###########################################################################
+"
