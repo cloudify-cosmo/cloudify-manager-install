34b84a1c79ab0ae5c3b4626a0838ebd233d8722b
Pavel Brodsky <mcouthon@gmail.com>
2018-02-07 12:05:38 +0200

    CFY-7859 Reword the post-rpm-install message

--- packaging/install.sh
+++ packaging/install.sh
@@ -3,14 +3,12 @@
 echo "
 ###########################################################################
 Cloudify installer is ready!
-To use it run:
+To install Cloudify Manager, run:
 cfy_manager install --private-ip <PRIVATE_IP> --public-ip <PUBLIC_IP>
 (Use cfy_manager -h for a full list of options)
 
-Alternatively, edit /etc/cloudify/config.yaml to set IPs and other options,
-and then run:
+You can specify more installation settings in /etc/cloudify/config.yaml. If you
+specify the public and private IP addresses in the config.yaml file, run:
 cfy_manager install
-
-(Members of the 'wheel' group have write-level access to this config file)
 ###########################################################################
 "
