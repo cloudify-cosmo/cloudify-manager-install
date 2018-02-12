c6ff1b52cca2be61812eee87eb40c5e1b8310e1e
Isaac Shabtay <isaac-s@users.noreply.github.com>
2018-02-12 13:46:14 +0200

    CFY-7875: use path, not name.. argh (#152)

--- cfy_manager/components/cli/cli.py
+++ cfy_manager/components/cli/cli.py
@@ -23,7 +23,7 @@ from ...config import config
 from ...logger import get_logger
 
 from ...utils import common
-from ...constants import EXTERNAL_CERT_FILENAME
+from ...constants import EXTERNAL_CERT_PATH
 from ...utils.install import yum_install, yum_remove
 
 
@@ -61,7 +61,7 @@ def _configure():
     set_cmd = [
         'cfy', 'profiles', 'set', '-u', username,
         '-p', password, '-t', 'default_tenant',
-        '-c', EXTERNAL_CERT_FILENAME, '--ssl', ssl_enabled,
+        '-c', EXTERNAL_CERT_PATH, '--ssl', ssl_enabled,
         '--skip-credentials-validation'
     ]
     logger.info('Setting CLI for default user...')
