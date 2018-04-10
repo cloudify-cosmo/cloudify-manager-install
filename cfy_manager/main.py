e4ae4e2ead5fb62347107ebfd8084a286e47c09f
idob-cloudify <33690603+idob-cloudify@users.noreply.github.com>
2018-04-10 09:49:46 +0300

    Update main.py

--- cfy_manager/main.py
+++ cfy_manager/main.py
@@ -315,15 +315,11 @@ def remove(verbose=False, force=False):
     _validate_force(force, 'remove')
     logger.notice('Removing Cloudify Manager...')
 
-<<<<<<< HEAD
-    for _, component in COMPONENTS:
-=======
     should_stop = _is_manager_installed()
 
-    for component in COMPONENTS.values():
+    for _, component in COMPONENTS:
         if should_stop and hasattr(component, 'stop'):
             component.stop()
->>>>>>> b5ff7609c13aefc55cb37c3c57f1f4f3895cb8e6
         component.remove()
 
     if _is_manager_installed():
