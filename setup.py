32eb159966a2a84d152145a0cb5d0390882861bf
opencm <limor@gigaspaces.com>
2018-06-13 21:48:37 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extra_files.append(join('..', 'config.yaml'))
 
 setup(
     name='cloudify-manager-install',
-    version='4.4.dev1',
+    version='4.4',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=find_packages(),
