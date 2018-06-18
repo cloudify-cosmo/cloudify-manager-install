b79e8516734ad9872e2485da4bab5fb0d70a9856
Pavel Brodsky <mcouthon@gmail.com>
2018-06-18 10:20:51 +0300

    CY-405 Properly setup logging for AMQP Postgres

--- cfy_manager/components/amqp_postgres/amqp_postgres.py
+++ cfy_manager/components/amqp_postgres/amqp_postgres.py
@@ -13,13 +13,24 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
+from os.path import join
+
+from .. import LOG_DIR_KEY
 from ..service_names import AMQP_POSTGRES
 
+from ...config import config
 from ...logger import get_logger
 from ...utils.systemd import systemd
+from ...constants import BASE_LOG_DIR
 
 
 logger = get_logger(AMQP_POSTGRES)
+
+
+def _setup_log_dir():
+    # Can't use AMQP_POSTGRES here because Jinja doesn't play nice with `-`s
+    conf = config.setdefault('amqp_postgres', {})
+    conf[LOG_DIR_KEY] = join(BASE_LOG_DIR, AMQP_POSTGRES)
 
 
 def _configure():
@@ -35,6 +46,7 @@ def install():
 
 def configure():
     logger.notice('Configuring AMQP-Postgres...')
+    _setup_log_dir()
     _configure()
     logger.notice('AMQP-Postgres successfully configured')
 
