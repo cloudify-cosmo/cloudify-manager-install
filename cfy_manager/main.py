59d29b0fd9a1ebbac9bb327daa422fd98ac68a27
Ohad <ohadbaruch1@gmail.com>
2018-08-08 12:06:11 +0300

    test

--- cfy_manager/main.py
+++ cfy_manager/main.py
@@ -129,6 +129,9 @@ def _load_config_and_logger(verbose=False,
     validate_config_access(config_write_required)
     config.load_config()
     manager_config = config[MANAGER]
+    logger.notice(config)
+    logger.notice(MANAGER)
+    logger.notice(manager_config)
 
     # If the DB wasn't initiated even once yet, always set clean_db to True
     config[CLEAN_DB] = clean_db or not _is_manager_installed()
@@ -266,7 +269,10 @@ def install(verbose=False,
         config_write_required=True
     )
 
+    logger.notice(config)
+    logger.notice(MANAGER)
     manager_config = config[MANAGER]
+    logger.notice(manager_config)
     if not manager_config[MANAGER][INSTALL_DATABASE_ONLY]:
         logger.notice('Installing Cloudify Manager...')
         validate(components=components_objects)
