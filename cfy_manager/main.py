ee03f3e983d819a53019249f33d928c2c5a7a299
Ohad <ohadbaruch1@gmail.com>
2018-08-13 13:07:02 +0300

    blaaaa

--- cfy_manager/main.py
+++ cfy_manager/main.py
@@ -217,7 +217,7 @@ def _get_services_to_install():
     else:
         for service in services_to_install:
             services_components_to_install += SERVICE_COMPONENTS[service]
-    logger.notice('Main-Temp: ' + services_components_to_install)
+    logger.notice('Main-Temp: ' + str(services_components_to_install))
     return services_components_to_install
 
 
