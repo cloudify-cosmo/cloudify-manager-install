1d78c957cf0684fa699a9218e2e4d543220a1139
asdf <tehasdf@gmail.com>
2019-02-28 18:25:04 +0100

    Scope is an array

--- cfy_manager/components/restservice/manager_config.py
+++ cfy_manager/components/restservice/manager_config.py
@@ -28,14 +28,14 @@ def make_manager_config():
         ConfigItem(
             'rest_service_log_path',
             config['restservice']['log_dir'] + '/cloudify-rest-service.log',
-            REST_SCOPE,
+            [REST_SCOPE],
             None,
             False
         ),
         ConfigItem(
             'rest_service_log_level',
             config['restservice']['log']['level'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'string', 'enum': ['DEBUG', 'INFO', 'WARNING',
                                         'ERROR', 'CRITICAL']},
             True
@@ -43,112 +43,112 @@ def make_manager_config():
         ConfigItem(
             'ldap_server',
             config['restservice']['ldap']['server'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'string'},
             True
         ),
         ConfigItem(
             'ldap_username',
             config['restservice']['ldap']['username'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'string'},
             True
         ),
         ConfigItem(
             'ldap_password',
             config['restservice']['ldap']['password'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'string'},
             True
         ),
         ConfigItem(
             'ldap_domain',
             config['restservice']['ldap']['domain'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'string'},
             True
         ),
         ConfigItem(
             'ldap_is_active_directory',
             config['restservice']['ldap']['is_active_directory'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'boolean'},
             True
         ),
         ConfigItem(
             'ldap_dn_extra',
             config['restservice']['ldap']['dn_extra'],
-            REST_SCOPE,
+            [REST_SCOPE],
             None,
             True
         ),
         ConfigItem(
             'ldap_timeout',
             5.0,
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'number'},
             True
         ),
         ConfigItem(
             'file_server_root',
             config['manager']['file_server_root'],
-            REST_SCOPE,
+            [REST_SCOPE],
             None,
             False
         ),
         ConfigItem(
             'file_server_url',
             config['manager']['file_server_url'],
-            REST_SCOPE,
+            [REST_SCOPE],
             None,
             False
         ),
         ConfigItem(
             'insecure_endpoints_disabled',
             config['restservice']['insecure_endpoints_disabled'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'boolean'},
             False
         ),
         ConfigItem(
             'maintenance_folder',
             config['restservice']['home_dir'] + '/maintenance',
-            REST_SCOPE,
+            [REST_SCOPE],
             None,
             False
         ),
         ConfigItem(
             'min_available_memory_mb',
             config['restservice']['min_available_memory_mb'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'number', 'minimum': 0},
             True
         ),
         ConfigItem(
             'failed_logins_before_account_lock',
             config['restservice']['failed_logins_before_account_lock'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'number', 'minimum': 1},
             True
         ),
         ConfigItem(
             'account_lock_period',
             config['restservice']['account_lock_period'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'number', 'minimum': -1},
             True
         ),
         ConfigItem(
             'public_ip',
             config['manager']['public_ip'],
-            REST_SCOPE,
+            [REST_SCOPE],
             None,
             False
         ),
         ConfigItem(
             'default_page_size',
             config['restservice']['default_page_size'],
-            REST_SCOPE,
+            [REST_SCOPE],
             {'type': 'number', 'minimum': 1},
             True
         ),
@@ -156,14 +156,14 @@ def make_manager_config():
         ConfigItem(
             'mgmtworker_max_workers',
             config['mgmtworker']['max_workers'],
-            MGMTWORKER_SCOPE,
+            [MGMTWORKER_SCOPE],
             {'type': 'number', 'minimum': 1},
             True
         ),
         ConfigItem(
             'mgmtworker_min_workers',
             config['mgmtworker']['min_workers'],
-            MGMTWORKER_SCOPE,
+            [MGMTWORKER_SCOPE],
             {'type': 'number', 'minimum': 1},
             True
         )
