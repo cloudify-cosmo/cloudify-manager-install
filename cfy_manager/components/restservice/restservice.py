af86d3f8ba0b52763ba61af473717ad9219d321e
Adi Grabow <adigrabow@gmail.com>
2019-03-17 13:47:40 +0200

    Allow all users to see licenses

--- cfy_manager/components/restservice/restservice.py
+++ cfy_manager/components/restservice/restservice.py
@@ -191,7 +191,7 @@ class RestServiceComponent(BaseComponent):
         except urllib2.URLError as e:
             raise NetworkError(
                 'REST service returned an invalid response: {0}'.format(e))
-        if response.code != 200 and response.code != 401:
+        if response.code != 200 and response.code != 400:
             raise NetworkError(
                 'REST service returned an unexpected response: '
                 '{0}'.format(response.code)
