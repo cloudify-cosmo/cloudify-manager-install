bfa22e4bae65eb281971e23b5b4fc7da04c62d9c
barucoh <ohad@cloudify.co>
2019-04-15 17:54:48 +0300

    Removing old cluster mechanism (#469)

--- cfy_manager/components/usage_collector/scripts/collect_cloudify_uptime.py
+++ cfy_manager/components/usage_collector/scripts/collect_cloudify_uptime.py
@@ -9,12 +9,6 @@ from requests import post
 
 from manager_rest import premium_enabled
 
-try:
-    from cloudify_premium.ha import node_status
-    from cloudify_premium.ha.utils import is_master
-except ImportError:
-    node_status = {'initialized': False}
-
 
 MANAGER_ID_PATH = '/etc/cloudify/.id'
 CLOUDIFY_ENDPOINT_UPTIME_URL = 'https://api.cloudify.co/cloudifyUptime'
@@ -48,19 +42,6 @@ def _send_data(data):
     post(CLOUDIFY_ENDPOINT_UPTIME_URL, data=data)
 
 
-def _is_clustered():
-    return bool(node_status.get('initialized'))
-
-
-def _is_active_manager():
-    if _is_clustered():
-        try:
-            return is_master()
-        except Exception:
-            return False
-    return True
-
-
 def _create_manager_id_file():
     if path.exists(MANAGER_ID_PATH):
         with open(MANAGER_ID_PATH) as f:
@@ -74,10 +55,6 @@ def _create_manager_id_file():
 def main():
     logger.info('Uptime script started running')
     _create_manager_id_file()
-    if not _is_active_manager():
-        logger.info('Uptime script finished running because the manager is '
-                    'not active')
-        return
     data = {}
     _collect_metadata(data)
     _send_data(data)
