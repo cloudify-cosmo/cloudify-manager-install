5425efe66c9c27e1b0925c06b9213840dda2e6b5
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2019-06-12 15:25:31 +0200

    Make certs using generate-test-cert

--- .circleci/cluster/create_certs.sh
+++ .circleci/cluster/create_certs.sh
@@ -1,98 +1,22 @@
 #!/usr/bin/env bash
 
-openssl req \
-    -x509 \
-    -nodes \
-    -newkey rsa:2048 \
-    -batch \
-    -days 3650 \
-    -out ca_cert.pem \
-    -keyout ca_key.pem \
-    -config .circleci/cluster/ca_config
+function generate_test_cert {
+    san=$1
+    docker run --rm  -v $(pwd):/root/.cloudify-test-ca cfy_manager_image_preinstalled cfy_manager generate-test-cert -s $san
+}
 
-sed -e "s/CONTAINER_IP/${QUEUE_IP}/g" .circleci/cluster/csr_config.template > queue_cert_config
-openssl req \
-    -newkey rsa:2048 \
-    -nodes \
-    -batch \
-    -sha256 \
-    -config queue_cert_config \
-    -out queue_cert.csr \
-    -keyout queue_key.pem
-openssl x509 \
-    -days 3650 \
-    -sha256 \
-    -req \
-    -in queue_cert.csr \
-    -out queue_cert.pem \
-    -extensions v3_ext \
-    -extfile queue_cert_config \
-    -CA ca_cert.pem \
-    -CAkey ca_key.pem \
-    -CAcreateserial
+generate_test_cert $QUEUE_IP
+mv $QUEUE_IP.crt queue_cert.pem
+mv $QUEUE_IP.key queue_key.pem
 
+generate_test_cert $DB_IP
+mv $DB_IP.crt db_cert.pem
+mv $DB_IP.key db_key.pem
 
-sed -e "s/CONTAINER_IP/${DB_IP}/g" .circleci/cluster/csr_config.template > db_cert_config
-openssl req \
-    -newkey rsa:2048 \
-    -nodes \
-    -batch \
-    -sha256 \
-    -config db_cert_config \
-    -out db_cert.csr \
-    -keyout db_key.pem
-openssl x509 \
-    -days 3650 \
-    -sha256 \
-    -req \
-    -in db_cert.csr \
-    -out db_cert.pem \
-    -extensions v3_ext \
-    -extfile db_cert_config \
-    -CA ca_cert.pem \
-    -CAkey ca_key.pem \
-    -CAcreateserial
+generate_test_cert $MANAGER1_IP
+mv $MANAGER1_IP.crt db_client_1_cert.pem
+mv $MANAGER1_IP.key db_client_1_key.pem
 
-
-sed -e "s/CONTAINER_IP/${MANAGER1_IP}/g" .circleci/cluster/csr_config.template > db_client_1_cert_config
-openssl req \
-    -newkey rsa:2048 \
-    -nodes \
-    -batch \
-    -sha256 \
-    -config db_client_1_cert_config \
-    -out db_client_1.csr \
-    -keyout db_client_1_key.pem
-openssl x509 \
-    -days 3650 \
-    -sha256 \
-    -req \
-    -in db_client_1.csr \
-    -out db_client_1_cert.pem \
-    -extensions v3_ext \
-    -extfile db_client_1_cert_config \
-    -CA ca_cert.pem \
-    -CAkey ca_key.pem \
-    -CAcreateserial
-
-
-sed -e "s/CONTAINER_IP/${MANAGER2_IP}/g" .circleci/cluster/csr_config.template > db_client_2_cert_config
-openssl req \
-    -newkey rsa:2048 \
-    -nodes \
-    -batch \
-    -sha256 \
-    -config db_client_2_cert_config \
-    -out db_client_2.csr \
-    -keyout db_client_2_key.pem
-openssl x509 \
-    -days 3650 \
-    -sha256 \
-    -req \
-    -in db_client_2.csr \
-    -out db_client_2_cert.pem \
-    -extensions v3_ext \
-    -extfile db_client_2_cert_config \
-    -CA ca_cert.pem \
-    -CAkey ca_key.pem \
-    -CAcreateserial
+generate_test_cert $MANAGER2_IP
+mv $MANAGER2_IP.crt db_client_2_cert.pem
+mv $MANAGER2_IP.key db_client_2_key.pem
