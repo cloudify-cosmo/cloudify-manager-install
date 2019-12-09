463460e4a2492828384bc48be174ed668dcbb660
David Ginzbourg <davidginzbourg@gmail.com>
2019-12-09 18:12:12 +0200

    Add more optional words to the status validations

--- .circleci/cluster/validate_status.sh
+++ .circleci/cluster/validate_status.sh
@@ -4,7 +4,7 @@ set -eux
 validate_status() {
   manager_status="$(docker exec $1 cfy cluster status)"
   set +e
-  grep -iq -e 'fail' -e 'error' <<< "$manager_status" && (echo "Found a failure in the cluster status" && exit 1)
+  grep -iq -e 'inactive' -e 'degraded' -e 'fail' -e 'error' <<< "$manager_status" && (echo "Found a failure in the cluster status" && exit 1)
   set -e
 }
 
