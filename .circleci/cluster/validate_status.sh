bf16eab315e70818200feaa310192653e005a410
tyacbovi <3802463+tyacbovi@users.noreply.github.com>
2019-12-26 10:37:39 +0200

    CY-2126: Adding full suppler for cluster status in CI
    - Fixing cluster status check to verify the status is not failing and OK
    - In cluster check, adding configuration step for all required cluster nodes for their status reporters
    - Printing out failed to fetch rpm for the build

--- .circleci/cluster/validate_status.sh
+++ .circleci/cluster/validate_status.sh
@@ -2,9 +2,11 @@
 set -eux
 
 validate_status() {
+  echo "Testing cluster status in $1"
   manager_status="$(docker exec $1 cfy cluster status)"
   set +e
-  grep -iq -e 'inactive' -e 'degraded' -e 'fail' -e 'error' <<< "$manager_status" && (echo "Found a failure in the cluster status" && exit 1)
+  grep -iq -e 'inactive' -e 'degraded' -e 'fail' -e 'error' <<< "$manager_status" && { echo "Found a failure in the cluster status" && exit 1; }
+  (! grep -iq -e 'ok' <<< "$manager_status") && { echo "Also could not find a service in OK state in cluster status" && exit 1; }
   set -e
 }
 
