bd9de73e067a22ef2ca69f987fa0ea20c1705fbd
tyacbovi <3802463+tyacbovi@users.noreply.github.com>
2019-12-27 12:41:23 +0200

    circleci- adding full cluster node report

--- .circleci/cluster/validate_status.sh
+++ .circleci/cluster/validate_status.sh
@@ -4,9 +4,10 @@ set -eux
 validate_status() {
   echo "Testing cluster status in $1"
   manager_status="$(docker exec $1 cfy cluster status)"
+  manager_status_json="$(docker exec $1 cfy cluster status --json)"
   set +e
-  grep -iq -e 'inactive' -e 'degraded' -e 'fail' -e 'error' <<< "$manager_status" && { echo "Found a failure in the cluster status" && exit 1; }
-  (! grep -iq -e 'ok' <<< "$manager_status") && { echo "Also could not find a service in OK state in cluster status" && exit 1; }
+  grep -iq -e 'inactive' -e 'degraded' -e 'fail' -e 'error' <<< "$manager_status" && { echo "Found a failure in the cluster status, here's the full report:" && echo $manager_status_json && exit 1; }
+  (! grep -iq -e 'ok' <<< "$manager_status") && { echo "Also could not find a service in OK state in cluster status, here's the full report:" && echo $manager_status_json && exit 1; }
   set -e
 }
 
