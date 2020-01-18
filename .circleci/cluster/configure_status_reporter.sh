9ac183d2e5cd4391cc6aee3e1cf12e1e36b0e65b
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2020-01-19 00:20:02 +0100

    configure_status_reporter.sh: also start it

--- .circleci/cluster/configure_status_reporter.sh
+++ .circleci/cluster/configure_status_reporter.sh
@@ -5,7 +5,7 @@ configure_reporter() {
   echo "Configuring status reporter for $1"
   docker cp ca.crt $1:/tmp/rest_ca.pem
   docker exec $1 cfy_manager status-reporter configure --managers-ip $2 --token $3 --ca-path /tmp/rest_ca.pem
-
+  docker exec $1 cfy_manager status-reporter start
 }
 
 for var in $1
