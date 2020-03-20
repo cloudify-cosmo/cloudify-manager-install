c4bb9059a1a1c8afdcc8a5e3ba54880442362c58
Mateusz Neumann <mateumann@gmail.com>
2020-03-20 15:57:56 +0100

    apply libfuturize.fixes.fix_division_safe

--- cfy_manager/components/validations.py
+++ cfy_manager/components/validations.py
@@ -80,7 +80,7 @@ def _get_host_total_memory():
         memory = memfile.read()
     for attribute in memory.splitlines():
         if attribute.lower().startswith('memtotal'):
-            return int(attribute.split(':')[1].strip().split(' ')[0]) / 1024
+            return int(attribute.split(':')[1].strip().split(' ')[0]) // 1024
 
 
 def _get_available_host_disk_space():
