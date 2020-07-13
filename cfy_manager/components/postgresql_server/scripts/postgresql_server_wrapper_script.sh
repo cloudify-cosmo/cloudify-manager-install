f73b65467245b67f35f3b0c95f0384a1ac50d273
Mohammed AbuAisha <mohammeda@cloudify.co>
2020-07-13 15:57:37 +0300

    CY-3080 Redirect child processes to the main supervisord log file

--- cfy_manager/components/postgresql_server/scripts/postgresql_server_wrapper_script.sh
+++ cfy_manager/components/postgresql_server/scripts/postgresql_server_wrapper_script.sh
@@ -3,8 +3,7 @@ set -e
 
 function stop_postgres() {
   echo "Stopping postgres.."
-  echo "Stopping postgres.." >> /tmp/test.txt
-  /usr/pgsql-9.5/bin/pg_ctl stop -D /var/lib/pgsql/9.5/data/ -s -m fast &>> /tmp/test.txt
+  /usr/pgsql-9.5/bin/pg_ctl stop -D /var/lib/pgsql/9.5/data/ -s -m fast
 }
 
 trap 'stop_postgres' EXIT
