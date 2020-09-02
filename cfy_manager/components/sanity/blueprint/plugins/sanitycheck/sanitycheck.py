52173c2357b31fdd12c583a1eb1b8c41f9c43f96
Mateusz Neumann <mateumann@gmail.com>
2020-09-02 18:02:25 +0200

    Add agent uninstall in sanity-check (#918)
    * Add agent uninstall in sanity-check
    
    A `delete` step was added in sanity-check blueprint.  It mimics
    the `cloudify_agent.installer.LocalInstallerMixin.delete_agent` behaviour
    by calling `cfy-agent daemons delete` on the specific node and then removing
    the runner associated with the agent.
    
    * shutil.rmtree instead of subprocess.check_call

--- cfy_manager/components/sanity/blueprint/plugins/sanitycheck/sanitycheck.py
+++ cfy_manager/components/sanity/blueprint/plugins/sanitycheck/sanitycheck.py
@@ -1,5 +1,7 @@
 import subprocess
 import tempfile
+import shutil
+from os.path import expanduser
 
 from cloudify.decorators import operation
 
@@ -10,3 +12,16 @@ def install_agent(ctx, **_):
     with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
         f.write(install_agent_script)
     subprocess.check_call(['bash', f.name])
+
+
+@operation
+def uninstall_agent(ctx, **_):
+    daemon_delete_cmd = [
+        expanduser('~cfyuser/{0}/env/bin/cfy-agent'.format(ctx.instance.id)),
+        'daemons', 'delete', '--name', ctx.instance.id
+    ]
+    subprocess.check_call(daemon_delete_cmd,
+                          env={'CLOUDIFY_DAEMON_STORAGE_DIRECTORY':
+                               expanduser('~cfyuser/.cfy-agent/')})
+
+    shutil.rmtree(expanduser('~cfyuser/{0}'.format(ctx.instance.id)))
