0ddbb5e621ce1fc41a1ee860f1892f49783dfd18
Mateusz Neumann <mateumann@gmail.com>
2020-11-12 17:03:16 +0100

    RD-80 Use Prometheus rules to determine cluster status (#973)
    * Add process_manager labels to the *_service rules
    
    * Handle None scrape_interval

--- cfy_manager/components/prometheus/prometheus.py
+++ cfy_manager/components/prometheus/prometheus.py
@@ -636,8 +636,8 @@ def _deploy_alerts_configuration(number_of_http_probes, uninstalling):
 
 
 def _calculate_alert_for(scrape_interval):
-    if scrape_interval:
-        scrape_interval = '{0}'.format(scrape_interval).lower()
+    scrape_interval = '{0}'.format(scrape_interval).lower() if scrape_interval\
+        else ''
     m = re.match(r'^((\d+)s)?((\d+)ms)?', scrape_interval)
     if not m or not m.lastindex or m.lastindex < 1:
         return '15s'
