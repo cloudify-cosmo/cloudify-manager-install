c430476737f6aab8f1754877bb64bcc022c1f732
Madda <nobody@nowhere.local>
2020-11-16 15:38:24 +0000

    RD-72 Stop providing node name to rabbitmqctl
    It is already provided in the rabbitmq-env.conf, and is actually correct there.

--- cfy_manager/components/rabbitmq/rabbitmq.py
+++ cfy_manager/components/rabbitmq/rabbitmq.py
@@ -1,18 +1,3 @@
-#########
-# Copyright (c) 2017 GigaSpaces Technologies Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
 import os
 import json
 import time
@@ -106,8 +91,7 @@ class RabbitMQ(BaseComponent):
         service.reload(RABBITMQ, ignore_failure=True)
 
     def _rabbitmqctl(self, command, **kwargs):
-        nodename = config[RABBITMQ]['nodename']
-        base_command = [RABBITMQ_CTL, '-n', nodename]
+        base_command = [RABBITMQ_CTL]
         if config[RABBITMQ]['use_long_name']:
             base_command.append('--longnames')
         return sudo(base_command + command, **kwargs)
