33645856e6ea7e1c433195fc11f34599a5292244
Mateusz Neumann <mateumann@gmail.com>
2020-11-23 11:36:35 +0100

    Remove debugging from sepolicy_deploy.sh script

--- cfy_manager/components/nginx/scripts/sepolicy_deploy.sh
+++ cfy_manager/components/nginx/scripts/sepolicy_deploy.sh
@@ -3,19 +3,13 @@
 POLICY=${2}
 PORTS=${3}
 
-echo "Setting up SELinux cloudify policy" | tee -a /tmp/sepolicy_deploy.log
 cd "${1}" || exit 1
 
-echo "Cleaning up previous ${POLICY} policy." | tee -a /tmp/sepolicy_deploy.log
-/usr/sbin/semodule -d "${POLICY}" 2>&1 | tee -a /tmp/sepolicy_deploy.log
-
-echo "Working in $( pwd )" | tee -a /tmp/sepolicy_deploy.log
-/bin/checkmodule -M -m -o "${POLICY}.mod" "${POLICY}.te" 2>&1 | tee -a /tmp/sepolicy_deploy.log
-/bin/semodule_package -o "${POLICY}.pp" -m "${POLICY}.mod" 2>&1 | tee -a /tmp/sepolicy_deploy.log
-/usr/sbin/semodule -i "${POLICY}.pp" 2>&1 | tee -a /tmp/sepolicy_deploy.log
+/usr/sbin/semodule -d "${POLICY}"
+/bin/checkmodule -M -m -o "${POLICY}.mod" "${POLICY}.te"
+/bin/semodule_package -o "${POLICY}.pp" -m "${POLICY}.mod"
+/usr/sbin/semodule -i "${POLICY}.pp"
 
 for P in ${PORTS} ; do
-    /usr/sbin/semanage port -a -t "${POLICY}_port_t" -p tcp "${P}" 2>&1 | tee -a /tmp/sepolicy_deploy.log
+    /usr/sbin/semanage port -a -t "${POLICY}_port_t" -p tcp "${P}"
 done
-
-echo "DONE" | tee -a /tmp/sepolicy_deploy.log
