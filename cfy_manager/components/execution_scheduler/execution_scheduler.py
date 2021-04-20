546859720b1f75df2aa7da799e36745e8da596cb
glukhman <47591609+glukhman@users.noreply.github.com>
2021-04-20 13:06:24 +0300

    Rd 1986 fix rpm upgrade from 512 521 (#1124)
    * RD-1986 Configure exec.scheduler service on upgrade
    
    * RD-1986 Don't trouble prometheus certs on upgrade
    
    * RD-1986 Fix additional scheduler and prometheus issues

--- cfy_manager/components/execution_scheduler/execution_scheduler.py
+++ cfy_manager/components/execution_scheduler/execution_scheduler.py
@@ -1,12 +1,12 @@
 from os.path import join
 
-from ..components_constants import LOG_DIR_KEY
+from ..components_constants import LOG_DIR_KEY, HOME_DIR_KEY
 from ..base_component import BaseComponent
 from ..service_names import EXECUTION_SCHEDULER
 from ...config import config
 from ...logger import get_logger
 from ...utils import service
-from ...constants import BASE_LOG_DIR
+from ...constants import BASE_LOG_DIR, REST_HOME_DIR
 
 
 logger = get_logger(EXECUTION_SCHEDULER)
@@ -20,15 +20,22 @@ class ExecutionScheduler(BaseComponent):
         conf = config.setdefault('execution_scheduler', {})
         conf[LOG_DIR_KEY] = join(BASE_LOG_DIR, EXECUTION_SCHEDULER)
 
-    def configure(self):
+    @staticmethod
+    def _setup_rest_config_dir():
+        conf = config.setdefault('restservice', {})
+        conf[HOME_DIR_KEY] = REST_HOME_DIR
+
+    def configure(self, upgrade=False):
         logger.notice('Configuring execution scheduler...')
+        if upgrade:
+            self._setup_rest_config_dir()
         self._setup_log_dir()
         service.configure('cloudify-execution-scheduler')
         logger.notice('Execution scheduler successfully configured')
         self.start()
 
     def upgrade(self):
-        self.configure()
+        self.configure(upgrade=True)
 
     def remove(self):
         logger.notice('Removing execution scheduler...')
