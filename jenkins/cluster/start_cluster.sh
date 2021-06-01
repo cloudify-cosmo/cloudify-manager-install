5dd276f95b0cd3937c160b49467d679737579c47
asdf <tehasdf@gmail.com>
2021-06-01 13:22:31 +0200

    prefix all rendered yaml files with rendered_
    this will make it easier to remove them

--- jenkins/cluster/start_cluster.sh
+++ jenkins/cluster/start_cluster.sh
@@ -27,58 +27,58 @@ set -eux
 sed -e "s/CONTAINER_IP/${QUEUE1_IP}/g" \
     -e "s/QUEUE2_IP/${QUEUE2_IP}/" \
     -e "s/QUEUE3_IP/${QUEUE3_IP}/" \
-    queue1_config.yaml > queue_1_config.yaml
-cat queue_1_config.yaml
+    queue1_config.yaml > rendered_queue1_config.yaml
+cat rendered_queue1_config.yaml
 docker run -d \
   --name ${NODE1_NAME} \
   --network net1 --ip ${QUEUE1_IP} \
-  -v $(pwd)/queue_1_config.yaml:/etc/cloudify/config.yaml \
+  -v $(pwd)/rendered_queue1_config.yaml:/etc/cloudify/config.yaml \
   -v $(pwd)/queue1_key.pem:/etc/cloudify/queue_key.pem \
   -v $(pwd)/queue1_cert.pem:/etc/cloudify/queue_cert.pem \
   -v $(pwd)/ca.crt:/etc/cloudify/ca.pem \
   ${IMAGE}
 docker exec ${NODE1_NAME} cfy_manager wait-for-starter -c /etc/cloudify/config.yaml
-docker cp queue_1_config.yaml ${NODE1_NAME}:/etc/cloudify/queue_config.yaml
+docker cp rendered_queue1_config.yaml ${NODE1_NAME}:/etc/cloudify/queue_config.yaml
 # Prepare Queue2 on Node 2
 sed -e "s/CONTAINER_IP/${QUEUE2_IP}/g" \
     -e "s/QUEUE1_IP/${QUEUE1_IP}/" \
     -e "s/QUEUE3_IP/${QUEUE3_IP}/" \
-    queue2_config.yaml > queue_2_config.yaml
-cat queue_2_config.yaml
+    queue2_config.yaml > rendered_queue2_config.yaml
+cat rendered_queue2_config.yaml
 docker run -d \
   --name ${NODE2_NAME} \
   --network net1 --ip ${QUEUE2_IP} \
-  -v $(pwd)/queue_2_config.yaml:/etc/cloudify/config.yaml \
+  -v $(pwd)/rendered_queue2_config.yaml:/etc/cloudify/config.yaml \
   -v $(pwd)/queue2_key.pem:/etc/cloudify/queue_key.pem \
   -v $(pwd)/queue2_cert.pem:/etc/cloudify/queue_cert.pem \
   -v $(pwd)/ca.crt:/etc/cloudify/ca.pem \
   ${IMAGE}
 docker exec ${NODE2_NAME} cfy_manager wait-for-starter -c /etc/cloudify/config.yaml
-docker cp queue_2_config.yaml ${NODE2_NAME}:/etc/cloudify/queue_config.yaml
+docker cp rendered_queue2_config.yaml ${NODE2_NAME}:/etc/cloudify/queue_config.yaml
 # Prepare Queue3 on Node 3
 sed -e "s/CONTAINER_IP/${QUEUE3_IP}/g" \
     -e "s/QUEUE1_IP/${QUEUE1_IP}/" \
     -e "s/QUEUE2_IP/${QUEUE2_IP}/" \
-    queue3_config.yaml > queue_3_config.yaml
-cat queue_3_config.yaml
+    queue3_config.yaml > rendered_queue3_config.yaml
+cat rendered_queue3_config.yaml
 docker run -d \
    --name ${NODE3_NAME} \
    --network net1 --ip ${QUEUE3_IP} \
-   -v $(pwd)/queue_3_config.yaml:/etc/cloudify/config.yaml \
+   -v $(pwd)/rendered_queue3_config.yaml:/etc/cloudify/config.yaml \
    -v $(pwd)/queue3_key.pem:/etc/cloudify/queue_key.pem \
    -v $(pwd)/queue3_cert.pem:/etc/cloudify/queue_cert.pem \
    -v $(pwd)/ca.crt:/etc/cloudify/ca.pem \
   ${IMAGE}
 docker exec ${NODE3_NAME} cfy_manager wait-for-starter -c /etc/cloudify/config.yaml
-docker cp queue_3_config.yaml ${NODE3_NAME}:/etc/cloudify/queue_config.yaml
+docker cp rendered_queue3_config.yaml ${NODE3_NAME}:/etc/cloudify/queue_config.yaml
 # Prepare DB1 on Node1
 sed -e "s/CONTAINER_IP/${DB1_IP}/g" \
     -e "s/DB1_IP/${DB1_IP}/g" \
     -e "s/DB2_IP/${DB2_IP}/g" \
     -e "s/DB3_IP/${DB3_IP}/g" \
-    db_config.yaml > db1_config.yaml
-cat db1_config.yaml
-sudo docker cp db1_config.yaml ${NODE1_NAME}:/etc/cloudify/db_config.yaml
+    db_config.yaml > rendered_db1_config.yaml
+cat rendered_db1_config.yaml
+sudo docker cp rendered_db1_config.yaml ${NODE1_NAME}:/etc/cloudify/db_config.yaml
 sudo docker cp db1_key.pem ${NODE1_NAME}:/etc/cloudify/db_key.pem
 sudo docker cp db1_cert.pem ${NODE1_NAME}:/etc/cloudify/db_cert.pem
 sudo docker exec ${NODE1_NAME} cfy_manager configure -c  /etc/cloudify/db_config.yaml
@@ -87,9 +87,9 @@ sed -e "s/CONTAINER_IP/${DB2_IP}/g" \
     -e "s/DB1_IP/${DB1_IP}/g" \
     -e "s/DB2_IP/${DB2_IP}/g" \
     -e "s/DB3_IP/${DB3_IP}/g" \
-    db_config.yaml > db2_config.yaml
-cat db2_config.yaml
-sudo docker cp db2_config.yaml ${NODE2_NAME}:/etc/cloudify/db_config.yaml
+    db_config.yaml > rendered_db2_config.yaml
+cat rendered_db2_config.yaml
+sudo docker cp rendered_db2_config.yaml ${NODE2_NAME}:/etc/cloudify/db_config.yaml
 sudo docker cp db2_key.pem ${NODE2_NAME}:/etc/cloudify/db_key.pem
 sudo docker cp db2_cert.pem ${NODE2_NAME}:/etc/cloudify/db_cert.pem
 sudo docker exec ${NODE2_NAME} cfy_manager configure -c /etc/cloudify/db_config.yaml -v
@@ -98,9 +98,9 @@ sed -e "s/CONTAINER_IP/${DB3_IP}/g" \
     -e "s/DB1_IP/${DB1_IP}/g" \
     -e "s/DB2_IP/${DB2_IP}/g" \
     -e "s/DB3_IP/${DB3_IP}/g" \
-    db_config.yaml > db3_config.yaml
-cat db3_config.yaml
-sudo docker cp db3_config.yaml ${NODE3_NAME}:/etc/cloudify/db_config.yaml
+    db_config.yaml > rendered_db3_config.yaml
+cat rendered_db3_config.yaml
+sudo docker cp rendered_db3_config.yaml ${NODE3_NAME}:/etc/cloudify/db_config.yaml
 sudo docker cp db3_key.pem ${NODE3_NAME}:/etc/cloudify/db_key.pem
 sudo docker cp db3_cert.pem ${NODE3_NAME}:/etc/cloudify/db_cert.pem
 sudo docker exec ${NODE3_NAME} cfy_manager configure -c /etc/cloudify/db_config.yaml -v
@@ -112,11 +112,11 @@ sed -e "s/CONTAINER_IP/${MANAGER1_IP}/g" \
     -e "s/DB1_IP/${DB1_IP}/g" \
     -e "s/DB2_IP/${DB2_IP}/g" \
     -e "s/DB3_IP/${DB3_IP}/g" \
-    manager1_config.yaml > manager_1_config.yaml
-cat manager_1_config.yaml
+    manager1_config.yaml > rendered_manager1_config.yaml
+cat rendered_manager1_config.yaml
   # Generate ca encrypted key
 openssl rsa -aes256 -passout pass:secret_ca_password -in ca.key -out ca.encrypted.key
-sudo docker cp manager_1_config.yaml ${NODE1_NAME}:/etc/cloudify/manager_config.yaml
+sudo docker cp rendered_manager1_config.yaml ${NODE1_NAME}:/etc/cloudify/manager_config.yaml
 sudo docker cp manager_1_key.pem ${NODE1_NAME}:/etc/cloudify/manager_key.pem
 sudo docker cp manager_1_cert.pem ${NODE1_NAME}:/etc/cloudify/manager_cert.pem
 sudo docker cp db_client_1_cert.pem ${NODE1_NAME}:/etc/cloudify/manager_postgres_client_cert.pem
@@ -133,9 +133,9 @@ sed -e "s/CONTAINER_IP/${MANAGER2_IP}/g" \
     -e "s/DB1_IP/${DB1_IP}/g" \
     -e "s/DB2_IP/${DB2_IP}/g" \
     -e "s/DB3_IP/${DB3_IP}/g" \
-    manager2_config.yaml > manager_2_config.yaml
-cat manager_2_config.yaml
-sudo docker cp manager_2_config.yaml ${NODE2_NAME}:/etc/cloudify/manager_config.yaml
+    manager2_config.yaml > rendered_manager2_config.yaml
+cat rendered_manager2_config.yaml
+sudo docker cp rendered_manager2_config.yaml ${NODE2_NAME}:/etc/cloudify/manager_config.yaml
 sudo docker cp manager_2_key.pem ${NODE2_NAME}:/etc/cloudify/manager_key.pem
 sudo docker cp manager_2_cert.pem ${NODE2_NAME}:/etc/cloudify/manager_cert.pem
 sudo docker cp db_client_2_cert.pem ${NODE2_NAME}:/etc/cloudify/manager_postgres_client_cert.pem
@@ -154,9 +154,9 @@ sed -e "s/CONTAINER_IP/${MANAGER3_IP}/g" \
     -e "s/DB1_IP/${DB1_IP}/g" \
     -e "s/DB2_IP/${DB2_IP}/g" \
     -e "s/DB3_IP/${DB3_IP}/g" \
-    manager3_config.yaml > manager_3_config.yaml
-cat manager_3_config.yaml
-sudo docker cp manager_3_config.yaml ${NODE3_NAME}:/etc/cloudify/manager_config.yaml
+    manager3_config.yaml > rendered_manager3_config.yaml
+cat rendered_manager3_config.yaml
+sudo docker cp rendered_manager3_config.yaml ${NODE3_NAME}:/etc/cloudify/manager_config.yaml
 sudo docker cp manager_3_key.pem ${NODE3_NAME}:/etc/cloudify/manager_key.pem
 sudo docker cp manager_3_cert.pem ${NODE3_NAME}:/etc/cloudify/manager_cert.pem
 sudo docker cp db_client_3_cert.pem ${NODE3_NAME}:/etc/cloudify/manager_postgres_client_cert.pem
