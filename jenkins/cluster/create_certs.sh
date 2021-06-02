f76f1feb4a0a5de19a19d5c5586defbb9a856149
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2021-06-02 17:04:02 +0200

    jenkins: move creating a cluster to a separate .sh (#1176)
    * Jenkinsfile: move starting the cluster to an outside script
    
    This script is going to be useful to be called by hand as well.
    And it's easier to maintain outside of the jenkinsfile anyway.
    
    * add the usual bash prelude
    
    * parametrize the script
    
    instead of jenkins-specific ${env} references, use arguments to the
    script, and have reasonable defaults as well
    
    * prefix all rendered yaml files with rendered_
    
    this will make it easier to remove them
    
    * remove unused `export`s
    
    create_certs is sourced anyway, not executed
    
    * put everything in functions
    
    i dont really want to refactor it further right now.
    But this is a start.
    
    * move everything into a tmpdir
    
    makes for way easier cleanup
    
    * create container names from prefix for validate_status.sh
    
    * jenkinsfile: drop the "cluster image" dockerfile
    
    not needed. We are just using the same image anyway. That one does
    `cfy_manager install --only install` in it.
    
    * drop unused file
    
    it even referenced a file in .circleci/ which doesn't exist
    
    * drop .circleci
    
    well unused for quite a while

--- jenkins/cluster/create_certs.sh
+++ jenkins/cluster/create_certs.sh
@@ -1,74 +1,76 @@
-#!/usr/bin/env bash
-
+# this script must be sourced from start_cluster.sh
 function generate_test_cert {
     san=$1
-    docker run --rm  -v $(pwd):/root/.cloudify-test-ca cfy_manager_image_preinstalled cfy_manager generate-test-cert -s $san
+    docker run --rm  -v $(pwd):/root/.cloudify-test-ca ${IMAGE} cfy_manager generate-test-cert -s $san
 }
 
-generate_test_cert $QUEUE1_IP
-mv $QUEUE1_IP.crt queue1_cert.pem
-mv $QUEUE1_IP.key queue1_key.pem
+function generate_certs(){
+    generate_test_cert $QUEUE1_IP
+    mv $QUEUE1_IP.crt queue1_cert.pem
+    mv $QUEUE1_IP.key queue1_key.pem
 
-generate_test_cert $QUEUE2_IP
-mv $QUEUE2_IP.crt queue2_cert.pem
-mv $QUEUE2_IP.key queue2_key.pem
+    generate_test_cert $QUEUE2_IP
+    mv $QUEUE2_IP.crt queue2_cert.pem
+    mv $QUEUE2_IP.key queue2_key.pem
 
-generate_test_cert $QUEUE3_IP
-mv $QUEUE3_IP.crt queue3_cert.pem
-mv $QUEUE3_IP.key queue3_key.pem
+    generate_test_cert $QUEUE3_IP
+    mv $QUEUE3_IP.crt queue3_cert.pem
+    mv $QUEUE3_IP.key queue3_key.pem
 
-generate_test_cert $DB1_IP
-mv $DB1_IP.crt db1_cert.pem
-mv $DB1_IP.key db1_key.pem
+    generate_test_cert $DB1_IP
+    mv $DB1_IP.crt db1_cert.pem
+    mv $DB1_IP.key db1_key.pem
 
-generate_test_cert $DB2_IP
-mv $DB2_IP.crt db2_cert.pem
-mv $DB2_IP.key db2_key.pem
+    generate_test_cert $DB2_IP
+    mv $DB2_IP.crt db2_cert.pem
+    mv $DB2_IP.key db2_key.pem
 
-generate_test_cert $DB3_IP
-mv $DB3_IP.crt db3_cert.pem
-mv $DB3_IP.key db3_key.pem
+    generate_test_cert $DB3_IP
+    mv $DB3_IP.crt db3_cert.pem
+    mv $DB3_IP.key db3_key.pem
 
-generate_test_cert $MANAGER1_IP
-mv $MANAGER1_IP.crt db_client_1_cert.pem
-mv $MANAGER1_IP.key db_client_1_key.pem
+    generate_test_cert $MANAGER1_IP
+    mv $MANAGER1_IP.crt db_client_1_cert.pem
+    mv $MANAGER1_IP.key db_client_1_key.pem
 
-generate_test_cert $MANAGER2_IP
-mv $MANAGER2_IP.crt db_client_2_cert.pem
-mv $MANAGER2_IP.key db_client_2_key.pem
+    generate_test_cert $MANAGER2_IP
+    mv $MANAGER2_IP.crt db_client_2_cert.pem
+    mv $MANAGER2_IP.key db_client_2_key.pem
 
-generate_test_cert $MANAGER3_IP
-mv $MANAGER3_IP.crt db_client_3_cert.pem
-mv $MANAGER3_IP.key db_client_3_key.pem
+    generate_test_cert $MANAGER3_IP
+    mv $MANAGER3_IP.crt db_client_3_cert.pem
+    mv $MANAGER3_IP.key db_client_3_key.pem
 
-generate_test_cert $MANAGER1_IP
-mv $MANAGER1_IP.crt external_cert_1.pem
-mv $MANAGER1_IP.key external_key_1.pem
+    generate_test_cert $MANAGER1_IP
+    mv $MANAGER1_IP.crt external_cert_1.pem
+    mv $MANAGER1_IP.key external_key_1.pem
 
-generate_test_cert $MANAGER2_IP
-mv $MANAGER2_IP.crt external_cert_2.pem
-mv $MANAGER2_IP.key external_key_2.pem
+    generate_test_cert $MANAGER2_IP
+    mv $MANAGER2_IP.crt external_cert_2.pem
+    mv $MANAGER2_IP.key external_key_2.pem
 
-generate_test_cert $MANAGER3_IP
-mv $MANAGER3_IP.crt external_cert_3.pem
-mv $MANAGER3_IP.key external_key_3.pem
+    generate_test_cert $MANAGER3_IP
+    mv $MANAGER3_IP.crt external_cert_3.pem
+    mv $MANAGER3_IP.key external_key_3.pem
 
-generate_test_cert $MANAGER1_IP
-mv $MANAGER1_IP.crt manager_1_cert.pem
-mv $MANAGER1_IP.key manager_1_key.pem
+    generate_test_cert $MANAGER1_IP
+    mv $MANAGER1_IP.crt manager_1_cert.pem
+    mv $MANAGER1_IP.key manager_1_key.pem
 
-generate_test_cert $MANAGER2_IP
-mv $MANAGER2_IP.crt manager_2_cert.pem
-mv $MANAGER2_IP.key manager_2_key.pem
+    generate_test_cert $MANAGER2_IP
+    mv $MANAGER2_IP.crt manager_2_cert.pem
+    mv $MANAGER2_IP.key manager_2_key.pem
 
-generate_test_cert $MANAGER3_IP
-mv $MANAGER3_IP.crt manager_3_cert.pem
-mv $MANAGER3_IP.key manager_3_key.pem
+    generate_test_cert $MANAGER3_IP
+    mv $MANAGER3_IP.crt manager_3_cert.pem
+    mv $MANAGER3_IP.key manager_3_key.pem
 
-generate_test_cert $MANAGER2_IP
-mv $MANAGER2_IP.crt prometheus_cert_2.pem
-mv $MANAGER2_IP.key prometheus_key_2.pem
+    generate_test_cert $MANAGER2_IP
+    mv $MANAGER2_IP.crt prometheus_cert_2.pem
+    mv $MANAGER2_IP.key prometheus_key_2.pem
 
-generate_test_cert $MANAGER3_IP
-mv $MANAGER3_IP.crt prometheus_cert_3.pem
-mv $MANAGER3_IP.key prometheus_key_3.pem
+    generate_test_cert $MANAGER3_IP
+    mv $MANAGER3_IP.crt prometheus_cert_3.pem
+    mv $MANAGER3_IP.key prometheus_key_3.pem
+
+}
