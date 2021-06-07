5ae1e38af8f03a44987790f73e472b29c8bd40e1
Mateusz Neumann <mateumann@gmail.com>
2021-06-07 11:47:21 +0200

    Fix PostgreSQL host split also for IPv6 addresses

--- cfy_manager/components/validations.py
+++ cfy_manager/components/validations.py
@@ -555,7 +555,7 @@ def _validate_postgres_inputs():
                                   'postgres_password must be set')
 
     if is_installed(MANAGER_SERVICE) and not is_installed(DATABASE_SERVICE):
-        postgres_host = config[POSTGRESQL_CLIENT]['host'].split(':')[0]
+        postgres_host = config[POSTGRESQL_CLIENT]['host'].rsplit(':', 1)[0]
         if postgres_host in ('localhost', '127.0.0.1') and \
                 not config[POSTGRESQL_CLIENT][SERVER_PASSWORD]:
             raise ValidationError('When using an external database, '
