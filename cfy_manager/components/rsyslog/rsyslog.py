f8695d7a12c838759a02ceab3d0fb17cc7e89a12
geokala <geokala@users.noreply.github.com>
2021-10-07 10:16:26 +0100

    Properly install/uninstall rsyslog (#1241)

---
+++ cfy_manager/components/rsyslog/rsyslog.py
@@ -0,0 +1,44 @@
+from cfy_manager.components.base_component import BaseComponent
+from cfy_manager.logger import get_logger
+from cfy_manager.utils import common, files, service, syslog
+
+SYSLOG_WRAPPER_PATH = '/opt/cloudify/syslog_wrapper_script.sh'
+
+logger = get_logger('rsyslog')
+
+
+class Rsyslog(BaseComponent):
+    component_name = 'rsyslog'
+
+    def configure(self):
+        if syslog.using_systemd_rsyslog():
+            logger.notice('Using system rsyslog')
+        logger.notice('Configuring Rsyslog...')
+        syslog_wrapper = '''#!/bin/bash
+set -e
+
+rm -f /var/run/syslogd.pid
+
+exec /usr/sbin/rsyslogd -n'''
+        files.write_to_file(syslog_wrapper, SYSLOG_WRAPPER_PATH)
+        common.chmod('755', SYSLOG_WRAPPER_PATH)
+        service.configure(
+            'rsyslog',
+            config_path='config/supervisord',
+        )
+
+    def remove(self):
+        if syslog.using_systemd_rsyslog():
+            # We don't manage this
+            return
+        logger.notice('Removing Rsyslog...')
+        files.remove_files([SYSLOG_WRAPPER_PATH])
+        service.remove('rsyslog')
+
+    # Logic to stop the logger is deliberately not included
+    def start(self):
+        if syslog.using_systemd_rsyslog():
+            # We don't manage this
+            return
+        logger.notice('Ensuring Rsyslog is running')
+        service.start('rsyslog')
