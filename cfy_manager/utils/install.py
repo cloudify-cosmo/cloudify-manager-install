d440155f9feedf23d86ad79eaa913ebe68fd1e01
geokala <geokala@users.noreply.github.com>
2021-12-23 14:00:41 +0000

    Keep yum output in order in case of errors (#1285)

--- cfy_manager/utils/install.py
+++ cfy_manager/utils/install.py
@@ -1,18 +1,5 @@
-#########
-# Copyright (c) 2017 GigaSpaces Technologies Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
 import re
+import subprocess
 
 from ..logger import get_logger
 
@@ -36,7 +23,7 @@ def _yum_install(packages, disable_all_repos=True):
     ] + packages
     if not disable_all_repos:
         install_cmd.remove('--disablerepo=*')
-    sudo(install_cmd)
+    sudo(install_cmd, stderr=subprocess.STDOUT)
 
 
 def yum_install(packages, disable_all_repos=True):
