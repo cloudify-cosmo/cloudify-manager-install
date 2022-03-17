6f8050f4b22cad352a1e5af504cd59ecc8445177
Madda <nobody@nowhere.local>
2022-03-17 09:44:16 +0000

    RD-4486 Use new tokens for mgmtworker

--- cfy_manager/components/mgmtworker/scripts/create-admin-token.py
+++ cfy_manager/components/mgmtworker/scripts/create-admin-token.py
@@ -1,48 +1,22 @@
-#!/opt/manager/env/bin/python
-#########
-# Copyright (c) 2018 Cloudify Platform Ltd. All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-
+#!/opt/cfy/bin/python
 import os
 import pwd
 import grp
 from stat import S_IREAD
+import socket
 
-from manager_rest import config, server, storage
+from cloudify_cli.env import get_rest_client
 
-RESTSERVICE_CONFIG_PATH = '/opt/manager/cloudify-rest.conf'
-RESTSEC_CONFIG_PATH = '/opt/manager/rest-security.conf'
-MANAGER_PYTHON = '/opt/manager/env/bin/python'
 AUTH_TOKEN_LOCATION = '/opt/mgmtworker/work/admin_token'
 
 
 def generate_auth_token():
-    config.instance.load_from_file(RESTSERVICE_CONFIG_PATH)
-    config.instance.rest_service_log_path = ''
-    app = server.CloudifyFlaskApp(load_config=False)
-    try:
-        with app.app_context():
-            sm = storage.get_storage_manager()
-
-            enc_uid = storage.idencoder.get_encoder().encode(0)
-
-            admin_user = sm.get(storage.models.User, 0)
-            token_key = admin_user.api_token_key
-
-            return enc_uid + token_key
-    finally:
-        config.reset(config.Config())
+    client = get_rest_client()
+    hostname = socket.gethostname()
+    token = client.tokens.create(
+        description=f'Mgmtworker token for {hostname}',
+    )
+    return token.value
 
 
 def update_auth_token(token):
@@ -55,9 +29,4 @@ def update_auth_token(token):
 
 
 if __name__ == '__main__':
-    if 'MANAGER_REST_SECURITY_CONFIG_PATH' not in os.environ:
-        os.environ['MANAGER_REST_SECURITY_CONFIG_PATH'] = (
-            RESTSEC_CONFIG_PATH
-        )
-
     update_auth_token(generate_auth_token())
