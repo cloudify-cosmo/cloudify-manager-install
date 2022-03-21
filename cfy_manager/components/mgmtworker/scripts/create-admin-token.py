7818207fa5c535b073915a33b9e0be80d7146bae
geokala <geokala@users.noreply.github.com>
2022-03-21 14:16:39 +0000

    RD-4497 Don't include hostname in mgmtworker token (#1338)
    * RD-4497 Don't include hostname in mgmtworker token
    
    We replicate the token anyway, so no need.
    
    * RD-4497 Don't use CLI profile for admin tokens
    
    After a snapshot restore this will break.
    
    * Use updated auth token creation

--- cfy_manager/components/mgmtworker/scripts/create-admin-token.py
+++ cfy_manager/components/mgmtworker/scripts/create-admin-token.py
@@ -1,25 +1,38 @@
-#!/opt/cfy/bin/python
+#!/opt/manager/env/bin/python
 import os
 import pwd
 import grp
 from stat import S_IREAD
-import socket
 
-from cloudify_cli.env import get_rest_client
+from manager_rest import config, server, storage
 
+RESTSERVICE_CONFIG_PATH = '/opt/manager/cloudify-rest.conf'
+RESTSEC_CONFIG_PATH = '/opt/manager/rest-security.conf'
 AUTH_TOKEN_LOCATION = '/opt/mgmtworker/work/admin_token'
 
 
 def generate_auth_token():
-    hostname = socket.gethostname()
-    description = f'Mgmtworker token for {hostname}'
-    client = get_rest_client()
+    config.instance.load_configuration(from_db=False)
+    config.instance.rest_service_log_path = ''
 
-    for existing in client.tokens.list(description=description):
-        client.tokens.delete(existing['id'])
+    description = 'csys-mgmtworker'
 
-    token = client.tokens.create(description=description)
-    return token.value
+    app = server.CloudifyFlaskApp(load_config=False)
+    try:
+        with app.app_context():
+            sm = storage.get_storage_manager()
+
+            # Don't leak existing Mgmtworker tokens
+            for tok in sm.list(storage.models.Token,
+                               filters={'description': description}):
+                sm.delete(tok)
+
+            admin = sm.get(storage.models.User, 0)
+            token = admin.create_auth_token(description=description)
+
+            return token.value
+    finally:
+        config.reset(config.Config())
 
 
 def update_auth_token(token):
@@ -32,4 +45,11 @@ def update_auth_token(token):
 
 
 if __name__ == '__main__':
+    if 'MANAGER_REST_CONFIG_PATH' not in os.environ:
+        os.environ['MANAGER_REST_CONFIG_PATH'] = \
+            RESTSERVICE_CONFIG_PATH
+    if 'MANAGER_REST_SECURITY_CONFIG_PATH' not in os.environ:
+        os.environ['MANAGER_REST_SECURITY_CONFIG_PATH'] = \
+            RESTSEC_CONFIG_PATH
+
     update_auth_token(generate_auth_token())
