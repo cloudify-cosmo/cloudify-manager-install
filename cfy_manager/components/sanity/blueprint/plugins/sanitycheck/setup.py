e346213b5711a98d01cbf1235383d38e90b989d3
opencm <limor@gigaspaces.com>
2023-03-08 09:38:46 +0000

    Bump version

--- cfy_manager/components/sanity/blueprint/plugins/sanitycheck/setup.py
+++ cfy_manager/components/sanity/blueprint/plugins/sanitycheck/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='cloudify-sanitycheck-plugin',
-    version='7.1.0.dev1',
+    version='7.0.0.dev1',
     py_modules=['sanitycheck']
 )
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extra_files.append(join('..', 'config.yaml'))
 
 setup(
     name='cloudify-manager-install',
-    version='7.1.0.dev1',
+    version='7.0.0.dev1',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=find_packages(),
